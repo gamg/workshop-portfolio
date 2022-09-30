42bf26d6f23142a2a1ae7c4e30c42d4d9a3855e5
gamg <adolfz10@gmail.com>
2022-09-30 14:05:24 -0400

    form for edit items was added

---
+++ resources/views/components/modals/slideover.blade.php
@@ -0,0 +1,33 @@
+<div x-data="{ open: @entangle('openSlideover').defer }"
+     @keydown.window.escape="open = false"
+     x-show="open" class="relative z-10" aria-labelledby="slide-over-title" x-ref="dialog" aria-modal="true">
+
+    <div x-show="open" x-transition:enter="ease-in-out duration-500" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in-out duration-500" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" x-description="Background backdrop, show/hide based on slide-over state." class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+    <div class="fixed inset-0 overflow-hidden">
+        <div class="absolute inset-0 overflow-hidden">
+            <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <div x-show="open" x-transition:enter="transform transition ease-in-out duration-500 sm:duration-700" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transform transition ease-in-out duration-500 sm:duration-700" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full" class="pointer-events-auto relative w-screen max-w-md" x-description="Slide-over panel, show/hide based on slide-over state." @click.away="open = false">
+                    <div x-show="open" x-transition:enter="ease-in-out duration-500" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in-out duration-500" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" x-description="Close button, show/hide based on slide-over state." class="absolute top-0 left-0 -ml-8 flex pt-4 pr-2 sm:-ml-10 sm:pr-4">
+                        <button type="button" class="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white" @click="open = false">
+                            <span class="sr-only">Close panel</span>
+                            <svg class="h-6 w-6" x-description="Heroicon name: outline/x" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                        <div class="px-4 sm:px-6">
+                            <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Panel</h2>
+                        </div>
+                        <div class="relative mt-6 flex-1 px-4 sm:px-6">
+                            <div class="w-full sm:max-w-md px-6 py-4">
+                                {{ $slot }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/navigation/navigation.blade.php
+++ resources/views/livewire/navigation/navigation.blade.php
@@ -24,10 +24,10 @@
                     </div>
 
                     <div class="hidden md:flex items-center justify-between space-x-2 ml-4 pb-1">
-                        <x-actions.action class="text-yellow-300 hover:text-blue-300" title="{{ __('Edit') }}">
+                        <x-actions.action wire:click.prevent="openSlide" class="text-yellow-300 hover:text-blue-300" title="{{ __('Edit') }}">
                             <x-icons.edit/>
                         </x-actions.action>
-                        <x-actions.action class="text-yellow-300 hover:text-blue-300" title="{{ __('New') }}">
+                        <x-actions.action wire:click.prevent="openSlide(true)" class="text-yellow-300 hover:text-blue-300" title="{{ __('New') }}">
                             <x-icons.add/>
                         </x-actions.action>
                     </div>
@@ -51,10 +51,10 @@
                     </div>
                     <div class="px-2 pt-2 pb-3 space-y-1">
                         <x-navigation.links class="block px-3 py-2 rounded-md text-base text-gray-700 hover:text-gray-900 hover:bg-gray-50" :items="$items"/>
-                        <x-actions.action class="block px-2 text-yellow-500 hover:text-blue-500" title="{{ __('Edit') }}">
+                        <x-actions.action wire:click.prevent="openSlide" class="block px-2 text-yellow-500 hover:text-blue-500" title="{{ __('Edit') }}">
                             <x-icons.edit/>
                         </x-actions.action>
-                        <x-actions.action class="block px-2 text-yellow-500 hover:text-blue-500" title="{{ __('New') }}">
+                        <x-actions.action wire:click.prevent="openSlide(true)" class="block px-2 text-yellow-500 hover:text-blue-500" title="{{ __('New') }}">
                             <x-icons.add/>
                         </x-actions.action>
                     </div>
@@ -62,6 +62,12 @@
             </div>
         </div>
 
-        <!-- slideover add/edit -->
+        <x-modals.slideover>
+            @if($addNewItem)
+                Formulario para agregar...
+            @else
+                Formulario para editar...
+            @endif
+        </x-modals.slideover>
     </section>
 </div>
