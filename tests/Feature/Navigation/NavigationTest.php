c557b8cdfbe01599e8c9e6487764c330b7236c39
gamg <adolfz10@gmail.com>
2022-10-01 12:40:08 -0400

    tests for items rules were added

--- tests/Feature/Navigation/NavigationTest.php
+++ tests/Feature/Navigation/NavigationTest.php
@@ -80,4 +80,60 @@ class NavigationTest extends TestCase
 
         $this->assertDatabaseMissing( 'navitems', ['id' => $item->id]);
     }
+
+    /** @test */
+    public function label_of_items_is_required()
+    {
+        $user = User::factory()->create();
+        $items = Navitem::factory(2)->create();
+
+        Livewire::actingAs($user)->test(Navigation::class)
+            ->set('items.0.label', '')
+            ->set('items.1.label', '')
+            ->call('edit')
+            ->assertHasErrors(['items.0.label' => 'required'])
+            ->assertHasErrors(['items.1.label' => 'required']);
+    }
+
+    /** @test */
+    public function link_of_items_is_required()
+    {
+        $user = User::factory()->create();
+        $items = Navitem::factory(2)->create();
+
+        Livewire::actingAs($user)->test(Navigation::class)
+            ->set('items.0.link', '')
+            ->set('items.1.link', '')
+            ->call('edit')
+            ->assertHasErrors(['items.0.link' => 'required'])
+            ->assertHasErrors(['items.1.link' => 'required']);
+    }
+
+    /** @test */
+    public function label_of_items_must_have_a_maximum_of_twenty_characters()
+    {
+        $user = User::factory()->create();
+        $items = Navitem::factory(2)->create();
+
+        Livewire::actingAs($user)->test(Navigation::class)
+            ->set('items.0.label', 'yehdyrgftgkyitjfhvngt')
+            ->set('items.1.label', 'yehdyrgftgkyitjfhvngt')
+            ->call('edit')
+            ->assertHasErrors(['items.0.label' => 'max'])
+            ->assertHasErrors(['items.1.label' => 'max']);
+    }
+
+    /** @test */
+    public function link_of_items_must_have_a_maximum_of_forty_characters()
+    {
+        $user = User::factory()->create();
+        $items = Navitem::factory(2)->create();
+
+        Livewire::actingAs($user)->test(Navigation::class)
+            ->set('items.0.link', 'yehdyrgftgkyitjfhvngtyehdyrgftgkyitjfhvngt')
+            ->set('items.1.link', 'yehdyrgftgkyitjfhvngtyehdyrgftgkyitjfhvngt')
+            ->call('edit')
+            ->assertHasErrors(['items.0.link' => 'max'])
+            ->assertHasErrors(['items.1.link' => 'max']);
+    }
 }
