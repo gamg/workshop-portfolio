727195dc86a333ceae41989c726194c5db438349
gamg <adolfz10@gmail.com>
2022-10-01 14:22:10 -0400

    tests were added for item component rules

--- tests/Feature/Navigation/ItemTest.php
+++ tests/Feature/Navigation/ItemTest.php
@@ -32,4 +32,52 @@ class ItemTest extends TestCase
 
         $this->assertDatabaseHas('navitems', ['label' => 'My label', 'link' => '#mylink']);
     }
+
+    /** @test */
+    public function label_is_required()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Item::class)
+            ->set('item.label', '')
+            ->set('item.link', '#mylink')
+            ->call('save')
+            ->assertHasErrors(['item.label' => 'required']);
+    }
+
+    /** @test */
+    public function label_must_have_a_maximum_of_twenty_characters()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Item::class)
+            ->set('item.label', 'jdhfjksdhfkhdskjfhkdhfkjdshfdsfhdj')
+            ->set('item.link', '#mylink')
+            ->call('save')
+            ->assertHasErrors(['item.label' => 'max']);
+    }
+
+    /** @test */
+    public function link_is_required()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Item::class)
+            ->set('item.label', 'Mi enlace')
+            ->set('item.link', '')
+            ->call('save')
+            ->assertHasErrors(['item.link' => 'required']);
+    }
+
+    /** @test */
+    public function link_must_have_a_maximum_of_forty_characters()
+    {
+        $user = User::factory()->create();
+
+        Livewire::actingAs($user)->test(Item::class)
+            ->set('item.label', 'Hello')
+            ->set('item.link', 'jdhfjksdhfkhdskjfhkdhfkjdshfdsfhdjkhdskjfhkdhfkjdshfdsfhdj')
+            ->call('save')
+            ->assertHasErrors(['item.link' => 'max']);
+    }
 }
