6ec43c0bf876e5174ddce6f2650ec9fb741651c0
gamg <adolfz10@gmail.com>
2022-11-14 14:42:18 -0400

    component can load projects

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(4),
+            'description' => $this->faker->text,
+            'image' => null,
+            'video_link' => null,
+            'url' => null,
+            'repo_url' => null,
+        ];
+    }
+}
---
+++ database/migrations/2022_11_14_151624_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->string('video_link')->nullable();
+            $table->string('url')->nullable();
+            $table->string('repo_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
