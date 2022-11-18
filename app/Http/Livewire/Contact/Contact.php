71b6cdc5630a6ac764db13fa69deb362752bab28
gamg <adolfz10@gmail.com>
2022-11-18 15:27:15 -0400

    component can load contact email

--- app/Http/Livewire/Contact/Contact.php
+++ app/Http/Livewire/Contact/Contact.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Livewire\Contact;
 
+use App\Models\PersonalInformation;
 use Livewire\Component;
 
 class Contact extends Component
 {
+    public PersonalInformation $contact;
+
+    public function mount()
+    {
+        $this->contact = PersonalInformation::select('email')->first() ?? new PersonalInformation();
+    }
+
     public function render()
     {
         return view('livewire..contact.contact');
--- tests/Feature/Contact/ContactTest.php
+++ tests/Feature/Contact/ContactTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature\Contact;
 
+use App\Http\Livewire\Contact\Contact;
+use App\Models\PersonalInformation;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class ContactTest extends TestCase
@@ -12,5 +15,13 @@ class ContactTest extends TestCase
     public function contact_component_can_be_rendered()
     {
         $this->get('/')->assertStatus(200)->assertSeeLivewire('contact.contact');
+    }
+
+    /** @test */
+    public function component_can_load_contact_email()
+    {
+        $info = PersonalInformation::factory()->create();
+
+        Livewire::test(Contact::class)->assertSee($info->email);
     }
 }
