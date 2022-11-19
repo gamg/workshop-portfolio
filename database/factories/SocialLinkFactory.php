c777de33328b175e5c4119fbbe28e3371caecbf0
gamg <adolfz10@gmail.com>
2022-11-18 20:25:59 -0400

    component can load social links

---
+++ database/factories/SocialLinkFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\SocialLink>
+ */
+class SocialLinkFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(1),
+            'url' => $this->faker->url,
+            'icon' => null,
+        ];
+    }
+}
--- resources/views/livewire/contact/social-link.blade.php
+++ resources/views/livewire/contact/social-link.blade.php
@@ -1,16 +1,13 @@
 <div class="flex justify-center mt-8 space-x-6">
-    <a href="#" target="_blank" class="text-4xl text-gray-400 hover:text-red-400">
-        <span class="sr-only">Linkedin</span>
-        <i class="fa-brands fa-linkedin"></i>
-    </a>
-    <a href="#" target="_blank" class="text-4xl text-gray-400 hover:text-red-400">
-        <span class="sr-only">Github</span>
-        <i class="fa-brands fa-github"></i>
-    </a>
-    <a href="#" target="_blank" class="text-4xl text-gray-400 hover:text-red-400">
-        <span class="sr-only">Twitter</span>
-        <i class="fa-brands fa-twitter"></i>
-    </a>
+    @forelse($socialLinks as $socialLink)
+        <a href="{{ $socialLink->url }}" target="_blank" class="text-4xl text-gray-400 hover:text-red-400">
+            <span class="{{ $socialLink->icon ? 'sr-only' : ''}}">{{ $socialLink->name }}</span>
+            <i class="{{ $socialLink->icon }}"></i>
+        </a>
+    @empty
+        <h3 class="text-gray-400">{{ __('There is no social links to show!') }}</h3>
+    @endforelse
+
     <!-- Boton add and edit -->
 
     <!-- SlideOver -->
