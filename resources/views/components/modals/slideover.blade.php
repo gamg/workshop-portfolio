3be507fd75d7cc5c4e3eb048b1abf5d8b9b33574
gamg <adolfz10@gmail.com>
2023-04-09 19:05:54 -0400

    style attr added to modal and slideover

--- resources/views/components/modals/slideover.blade.php
+++ resources/views/components/modals/slideover.blade.php
@@ -1,6 +1,7 @@
 <div x-data="{ open: @entangle('openSlideover').defer }"
      @keydown.window.escape="open = false"
-     x-show="open" class="relative z-10" aria-labelledby="slide-over-title" x-ref="dialog" aria-modal="true">
+     x-show="open" class="relative z-10" aria-labelledby="slide-over-title" x-ref="dialog" aria-modal="true"
+     style="display: none;">
 
     <div x-show="open" x-transition:enter="ease-in-out duration-500" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in-out duration-500" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" x-description="Background backdrop, show/hide based on slide-over state." class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
